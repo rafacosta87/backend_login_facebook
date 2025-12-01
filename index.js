eb17e646959cd45f6785b0fffb33f4aa34b19b1b
Rafael Costa <170887rafaelcostadasilva@gmail.com>
2025-12-01 00:29:54 -0300

    implementando tratamento de erros em endpoint login

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: '10mb' })) // Aumentar limite para imagens gran
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 const db = new sqlite3.Database("./database.db", (err) => {
-    if (err) console.error("Erro ao conectar o banco", err.message)
+  if (err) console.error("Erro ao conectar o banco", err.message)
 })
 
 db.run(`
@@ -43,7 +43,7 @@ const userSchemaBackend = yup.object().shape({
   data_nascimento: yup.string()
     .required('Data de nascimento é obrigatória')
     .test('is-valid-date', 'Data não existe', validateDate)
-    .test('is-past-date', 'Data de nascimento não pode ser no futuro ', function(value) {
+    .test('is-past-date', 'Data de nascimento não pode ser no futuro ', function (value) {
       const parsedDate = parse(value, dateFormat, new Date())
       // Permite data de hoje ou anterior
       return !isFuture(parsedDate)
@@ -54,15 +54,12 @@ const userSchemaBackend = yup.object().shape({
 });
 
 app.get("/", (req, res) => {
-    res.send("healthy")
+  res.send("healthy")
 })
 
 app.post('/usuario', async (req, res) => {
-   
   try {
-     
     const data = await userSchemaBackend.validate(req.body, { abortEarly: false })
-  
     db.get('SELECT email FROM usuarios WHERE email = ?', [data.email], (err, row) => {
       if (err) {
         return res.status(500).json({ error: { message: 'Erro no servidor' } })
@@ -75,16 +72,16 @@ app.post('/usuario', async (req, res) => {
           }],
         })
       }
-      const {  nome, sobrenome, data_nascimento, genero, email, senha, imagem  } = data
+      const { nome, sobrenome, data_nascimento, genero, email, senha, imagem } = data
       db.run(
         'INSERT INTO usuarios VALUES (NULL,?, ?, ?, ?, ?, ?, ?)',
-        [nome, sobrenome, data_nascimento, genero, email, senha, imagem], 
+        [nome, sobrenome, data_nascimento, genero, email, senha, imagem],
         function (err) {
           if (err) {
             console.log(err)
             return res.status(500).json({ error: { message: 'Erro ao salvar usuário' } })
           }
-          res.status(201).json({  nome, sobrenome, data_nascimento, genero, email, senha, id: this.lastID, imagem})
+          res.status(201).json({ nome, sobrenome, data_nascimento, genero, email, senha, id: this.lastID, imagem })
         }
       )
     })
@@ -98,106 +95,112 @@ app.post('/usuario', async (req, res) => {
     res.status(500).json({ message: 'Erro no servidor' })
   }
 });
-  // catch (err) {
-  //   // Lidar com erros de validação Yup no backend
-  //   if (err instanceof yup.ValidationError) {
-  //     // Retorna o primeiro erro de validação para o frontend processar ou um formato de erro genérico
-  //     return res.status(400).json({
-  //       error: {
-  //         field: err.path,
-  //         message: err.message,
-  //       },
-  //     });
-  //   }
-  //   res.status(500).json({ error: { message: 'Erro interno do servidor' } });
-  // }
+// catch (err) {
+//   // Lidar com erros de validação Yup no backend
+//   if (err instanceof yup.ValidationError) {
+//     // Retorna o primeiro erro de validação para o frontend processar ou um formato de erro genérico
+//     return res.status(400).json({
+//       error: {
+//         field: err.path,
+//         message: err.message,
+//       },
+//     });
+//   }
+//   res.status(500).json({ error: { message: 'Erro interno do servidor' } });
+// }
 
 app.get("/usuario", (req, res) => {
-    db.all(`SELECT * FROM usuarios `, [], (err, rows) => {
-        if (err) {
-            return res.status(400).json({ error: err.message })
-        }
-        res.json(rows)
-    })
+  db.all(`SELECT * FROM usuarios `, [], (err, rows) => {
+    if (err) {
+      return res.status(400).json({ error: err.message })
+    }
+    res.json(rows)
+  })
 })
 
 app.delete("/usuario/:id", (req, res) => {
-    const { id } = req.params
-    db.run(`DELETE FROM usuarios WHERE id = ?`,
-        [id],
-        (err, rows) => {
-            if (err) {
-                return res.status(400).json({ error: err.message })
-            }
-            if (!rows) {
-                return res.status(404).json({ error: "Usuário não encontrado" })
-            }
-            res.json({ message: "Usuário deletado com sucesso" })
-        }
-    )
+  const { id } = req.params
+  db.run(`DELETE FROM usuarios WHERE id = ?`,
+    [id],
+    (err, rows) => {
+      if (err) {
+        return res.status(400).json({ error: err.message })
+      }
+      if (!rows) {
+        return res.status(404).json({ error: "Usuário não encontrado" })
+      }
+      res.json({ message: "Usuário deletado com sucesso" })
+    }
+  )
 })
 
 app.get("/usuario/:id", (req, res) => {
-    const { id } = req.params
-    db.get(`SELECT * FROM usuarios WHERE id = ? `,
-        [id],
-        (err, rows) => {
-            if (err) {
-                return res.status(400).json({ error: err.message })
-            }
-            if (!rows) {
-                return res.status(404).json({ error: "Usuário não encontrado" })
-            }
-            res.json(rows)
-        }
-    )
+  const { id } = req.params
+  db.get(`SELECT * FROM usuarios WHERE id = ? `,
+    [id],
+    (err, rows) => {
+      if (err) {
+        return res.status(400).json({ error: err.message })
+      }
+      if (!rows) {
+        return res.status(404).json({ error: "Usuário não encontrado" })
+      }
+      res.json(rows)
+    }
+  )
 })
 
 app.post("/login", (req, res) => {
-
-    const { email, senha } = req.body
-    db.get('SELECT * FROM usuarios WHERE email=? AND senha=?',
-        [email, senha],
-        (err, rows) => {
-            if (err) {
-                return res.status(400).json({ error: err.message })
-            }
-            if (!rows) {
-                return res.status(404).json({ error: "Usuário não encontrado" })
-            }
-            res.json(rows)
-        }
-    )
-})
+  const { email, senha } = req.body;
+  if (!email) {
+    return res.status(400).json({ error: "Email é obrigatório" });
+  }
+  if (!senha) {
+    return res.status(402).json({ error: "Senha é obrigatório" });
+  }
+  db.get('SELECT * FROM usuarios WHERE email = ?', [email], (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+    if (!user) {
+      return res.status(404).json({ error: "Email não existe", field: "email" });
+    }
+    if (user.senha !== senha) {
+      // A senha está incorreta
+      return res.status(401).json({ error: "Senha incorreta" , field: "senha"});
+    }
+    res.status(200).json(user);
+  });
+});
 
 app.put("/atualizar/:id", (req, res) => {
 
-    // const { u} = req.query
-    const { id } = req.params
-    const { nome, sobrenome, data_nascimento, genero, email, senha, imagem } = req.body
-    db.get(
-        "SELECT * FROM usuarios WHERE id=?",
-        [id],
-        (err, row) => {
-            if (err) {
-                return res.status(400).json({ error: err.message })
-            }
-            if (!row) {
-                return res.status(404).json({ error: "Usuário não encontrado" })
-            }
-            db.run(
-                "UPDATE usuarios SET nome = ?, sobrenome = ?, data_nascimento = ?, genero = ?, email = ?, senha = ?, imagem= ? WHERE id = ?",
-                [nome ?? row?.nome, sobrenome ?? row?.sobrenome, data_nascimento ?? row?.data_nascimento, genero ?? row?.genero, email ?? row?.email, senha ?? row?.senha, imagem ?? row?.imagem, id]
+  // const { u} = req.query
+  const { id } = req.params
+  const { nome, sobrenome, data_nascimento, genero, email, senha, imagem } = req.body
+  db.get(
+    "SELECT * FROM usuarios WHERE id=?",
+    [id],
+    (err, row) => {
+      if (err) {
+        return res.status(400).json({ error: err.message })
+      }
+      if (!row) {
+        return res.status(404).json({ error: "Usuário não encontrado" })
+      }
+      db.run(
+        "UPDATE usuarios SET nome = ?, sobrenome = ?, data_nascimento = ?, genero = ?, email = ?, senha = ?, imagem= ? WHERE id = ?",
+        [nome ?? row?.nome, sobrenome ?? row?.sobrenome, data_nascimento ?? row?.data_nascimento, genero ?? row?.genero, email ?? row?.email, senha ?? row?.senha, imagem ?? row?.imagem, id]
 
-            );
-            res.json({ message: "usuario atualizado com sucesso" })
-        }
-    )
-    // // Status: OK
-    // return res.json(Usuario de id=${id} atualizado com sucesso!)
+      );
+      res.json({ message: "usuario atualizado com sucesso" })
+    }
+  )
+  // // Status: OK
+  // return res.json(Usuario de id=${id} atualizado com sucesso!)
 
 })
 
 app.listen(port, () => {
-    console.log(`servidor rodando em http://localhost:${port}`)
+  console.log(`servidor rodando em http://localhost:${port}`)
 })
